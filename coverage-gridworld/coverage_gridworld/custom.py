f24df8bcb42736ee916d2d4a8d9b1c554c784fec
allixf <allixfletcher@gmail.com>
2025-03-31 14:46:50 -0400

    reverted custom.py with switch to 255

--- coverage-gridworld/coverage_gridworld/custom.py
+++ coverage-gridworld/coverage_gridworld/custom.py
@@ -12,9 +12,10 @@ def observation_space(env: gym.Env) -> gym.spaces.Space:
     """
     # The grid has (10, 10, 3) shape and can store values from 0 to 255 (uint8). To use the whole grid as the
     # observation space, we can consider a MultiDiscrete space with values in the range [0, 256).
-    shape = env.grid.shape
-    nvec = np.full(shape, 256, dtype=np.int32)
-    return gym.spaces.MultiDiscrete(nvec.flatten())
+    cell_values = env.grid + 255
+
+    # if MultiDiscrete is used, it's important to flatten() numpy arrays!
+    return gym.spaces.MultiDiscrete(cell_values.flatten())
 
 
 def observation(grid: np.ndarray):
@@ -24,7 +25,7 @@ def observation(grid: np.ndarray):
     # If the observation returned is not the same shape as the observation_space, an error will occur!
     # Make sure to make changes to both functions accordingly.
 
-    return grid.flatten().astype(np.int32)
+    return grid.flatten()
 
 
 def reward(info: dict) -> float:
