0542e579f9b4e57e6b33d17563015d9a77d677f3
allixf <allixfletcher@gmail.com>
2025-03-31 15:28:44 -0400

    glitchy QRDQN start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import time
-import gymnasium
+import gymnasium as gym
 import coverage_gridworld  # must be imported, even though it's not directly referenced
 
 
@@ -89,18 +89,52 @@ maps = [
     ]
 ]
 
-env = gymnasium.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
-num_episodes = 5
+# env = gymnasium.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
+# num_episodes = 5
 
-for i in range(num_episodes):
-    env.reset()
-    done = False
-    while not done:
-        action = human_player()
-        obs, reward, done, truncated, info = env.step(action)
+# for i in range(num_episodes):
+#     env.reset()
+#     done = False
+#     while not done:
+#         action = human_player()
+#         obs, reward, done, truncated, info = env.step(action)
 
-        # Sleep may be used to allow each step to be visualized. Value can be changed
-        #time.sleep(0.2)
-    if done:
-        time.sleep(2)
+#         # Sleep may be used to allow each step to be visualized. Value can be changed
+#         #time.sleep(0.2)
+#     if done:
+#         time.sleep(2)
+# env.close()
+
+
+#-------
+
+# from stable_baselines3 import DQN
+# import simple_coverage
+from stable_baselines3.common.env_util import make_vec_env
+from sb3_contrib import QRDQN
+
+
+# Parallel environments for training
+#vec_env = make_vec_env("complete", n_envs=4)
+env = gym.make("just_go", render_mode="human", predefined_map_list=None, activate_game_status=True)
+policy_kwargs = dict(n_quantiles=50)
+model = QRDQN("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
+model.learn(total_timesteps=1000, log_interval=4)
+model.save("gridworld")
+
+# Single environment for testing
+env = gym.make("complete", render_mode="human")
+model = QRDQN.load("gridworld")
+obs, _ = env.reset()
+terminated = False
+total_reward = 0
+
+while not terminated:
+    action, _states = model.predict(obs, deterministic=True)
+    print(action)
+    obs, rewards, terminated, truncated, info = env.step(action)
+    total_reward += rewards
+    time.sleep(0.5)
+
+print(f"Total reward: {total_reward}")
 env.close()
